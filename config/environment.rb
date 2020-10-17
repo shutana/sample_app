c14a9ed0225b50efa4d642737dcea6f401455aff
Shusei Tanaka <shusei-tanaka@ShuseinoMacBook-Pro.local>
2020-10-17 10:31:02 +0900

    Use SSL and the Puma webserver in production

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ # Force all access to the app over SSL, use Strict-Transport-Security,
+  # and use secure cookies.
+  config.force_ssl = true
